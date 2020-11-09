29a3172d6b4edd8e1578a017f28e2de6e624529e
mrbdahlem <dahlem.brian@cusd80.com>
2020-11-08 18:49:38 -0700

    Now with a null test

---
+++ src/test/java/run/mycode/basiclti/BasicLtiApplicationTests.java
@@ -0,0 +1,12 @@
+package run.mycode.basiclti;
+
+import org.junit.jupiter.api.Test;
+
+class BasicLtiApplicationTests {
+
+    @Test
+    void nullTest() {
+    }
+    
+    //TODO: Need to test LTI Verification. how to mock an http connection for filter?
+}
