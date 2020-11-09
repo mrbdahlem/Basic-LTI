2333f9c7be0f1be8e23c838e5cb4862ed19d2ab6
mrbdahlem <dahlem.brian@cusd80.com>
2020-11-08 19:00:10 -0700

    Add this is not a test warning

--- src/test/java/run/mycode/basiclti/BasicLtiApplicationTests.java
+++ src/test/java/run/mycode/basiclti/BasicLtiApplicationTests.java
@@ -1,11 +1,16 @@
 package run.mycode.basiclti;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 class BasicLtiApplicationTests {
+    
+    private static final Logger LOG = LogManager.getLogger(BasicLtiApplicationTests.class);
 
     @Test
     void nullTest() {
+        LOG.warn("THIS IS NOT A TEST!!!");
     }
     
     //TODO: Need to test LTI Verification. how to mock an http connection for filter?
